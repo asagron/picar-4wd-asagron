eb696517a74e915d9b0111e9085b296eafce1a2f
Abrielle Agron <abrielleagron@MacBook-Air.local>
2024-01-22 21:00:26 -0600

    increased threshold

--- examples/roomba.py
+++ examples/roomba.py
@@ -6,8 +6,8 @@ speed = 10
 angle = 50
 
 def main():
-    threshold = 20
-    distance = 100
+    threshold = 30
+    distance = distance = check_view()
     while True:
         if distance < threshold:
             time.sleep(.2)
@@ -32,24 +32,6 @@ def check_view(fov = 40):
             distance = 100
         dist_list.append(distance)
     return (min(dist_list))
-
-            
-
-
-        # time.sleep(1)
-        # scan_list = fc.scan_step(35)
-        # if not scan_list:
-        #     continue
-
-        # tmp = scan_list[3:7]
-        # print(tmp)
-        # if tmp != [2,2,2,2]:
-        #     angle = random.randint(-30, 30)
-        #     fc.stop()
-        #     fc.backward(speed/2)
-        #     fc.turn_right(angle)
-        # else:
-        #     fc.forward(speed)
 
 if __name__ == "__main__":
     try: 
