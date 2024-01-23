7ac4eaf0323ba1fde3300e7833d8494abc8d64e6
Abrielle Agron <abrielleagron@MacBook-Air.local>
2024-01-22 19:57:29 -0600

    simplified roomba mode

--- examples/roomba.py
+++ examples/roomba.py
@@ -3,23 +3,33 @@ import time
 import random
 
 speed = 10
+angle = 50
 
 def main():
     while True:
-        # time.sleep(1)
-        scan_list = fc.scan_step(35)
-        if not scan_list:
-            continue
-
-        tmp = scan_list[3:7]
-        print(tmp)
-        if tmp != [2,2,2,2]:
-            angle = random.randint(-30, 30)
-            fc.stop()
-            fc.backward(speed/2)
-            fc.turn_right(angle)
-        else:
+        threshold = 10
+        distance = 100
+        while threshold < distance:
+            time.sleep(1)
+            distance = fc.get_distance_at(angle)
+            print(distance)
             fc.forward(speed)
+    print("done")
+
+        # time.sleep(1)
+        # scan_list = fc.scan_step(35)
+        # if not scan_list:
+        #     continue
+
+        # tmp = scan_list[3:7]
+        # print(tmp)
+        # if tmp != [2,2,2,2]:
+        #     angle = random.randint(-30, 30)
+        #     fc.stop()
+        #     fc.backward(speed/2)
+        #     fc.turn_right(angle)
+        # else:
+        #     fc.forward(speed)
 
 if __name__ == "__main__":
     try: 
