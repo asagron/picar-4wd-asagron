ace165dac25065f0a2b709b1aed9900a8e0321b6
Abrielle Agron <abrielleagron@MacBook-Air.local>
2024-01-22 19:18:59 -0600

    moved servo_set.py

--- examples/servo_set.py
+++ examples/servo_set.py
@@ -1,12 +1,11 @@
 import picar_4wd as fc
 import time
-from .servo import Servo
 
 try:
     while True:
         for i in range(-90, 90, 10):
             print(i)
-            fc.servo.set_angle(i)
+            fc.set_angle(i)
             time.sleep(1)
 finally:
     fc.stop()
