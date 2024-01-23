7bfd9cb2f3060f5da011edb8e04eceace50af44a
Abrielle Agron <abrielleagron@MacBook-Air.local>
2024-01-22 19:49:42 -0600

    removed sleep

--- examples/roomba.py
+++ examples/roomba.py
@@ -6,7 +6,7 @@ speed = 10
 
 def main():
     while True:
-        time.sleep(1)
+        # time.sleep(1)
         scan_list = fc.scan_step(35)
         if not scan_list:
             continue
@@ -14,7 +14,7 @@ def main():
         tmp = scan_list[3:7]
         print(tmp)
         if tmp != [2,2,2,2]:
-            angle = random.randint(-90, 90)
+            angle = random.randint(-30, 30)
             fc.stop()
             fc.backward(speed/2)
             fc.turn_right(angle)
