2617b3a8c5c9f90b846e6a317f8279923113a8b4
Abrielle Agron <abrielleagron@MacBook-Air.attlocal.net>
2024-01-27 15:05:46 -0600

    continuous measurements

--- examples/mapping.py
+++ examples/mapping.py
@@ -5,10 +5,11 @@ import random
 angle = 10
 
 def main():
-    for i in range (-60, 60, 60):
-        time.sleep(1)
-        distance = fc.get_distance_at(angle + i)
-        print(str(i), str(distance))
+    while(True):
+        for i in range (-60, 61, 60):
+            time.sleep(1)
+            distance = fc.get_distance_at(angle + i)
+            print(str(i), str(distance))
 if __name__ == "__main__":
     try: 
         main()
