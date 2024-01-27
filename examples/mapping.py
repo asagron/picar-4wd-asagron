1acf3be4bff2291e9d2569b4540a9b5ae4ffdb8c
Abrielle Agron <abrielleagron@MacBook-Air.attlocal.net>
2024-01-27 15:28:21 -0600

    added coords

--- examples/mapping.py
+++ examples/mapping.py
@@ -1,14 +1,18 @@
 import picar_4wd as fc
 import time
 import random
+import math
 
 angle = 10
 
 def main():
     while(True):
+        distance = fc.get_distance_at(angle-60)
         for i in range (-60, 61, 5):
             distance = fc.get_distance_at(angle + i)
-            print(str(i), str(distance))
+            x = distance * math.sin(i)
+            y = distance * math.cos(i)
+            print("Angle " + str(i) + ": " + str(distance) + "Coord: " + str(x) + "," + str(y))
             time.sleep(1)
 
 if __name__ == "__main__":
