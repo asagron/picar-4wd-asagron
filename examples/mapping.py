121e50cd65508eb9b46ac3ca185937a478d909df
Abrielle Agron <abrielleagron@MacBook-Air.attlocal.net>
2024-01-27 14:37:14 -0600

    fixed servo

--- examples/mapping.py
+++ examples/mapping.py
@@ -2,9 +2,13 @@ import picar_4wd as fc
 import time
 import random
 
+angle = 10
+
 def main():
-    distance = fc.get_distance_at(50)
-    print(distance)
+    for i in range (-60, 60, 60):
+        time.sleep(1)
+        distance = fc.get_distance_at(angle + i)
+        print(str(i), str(distance))
 if __name__ == "__main__":
     try: 
         main()
