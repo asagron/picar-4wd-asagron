b184bb426c7d008f84b4a6d3bb99845671d8deeb
Abrielle Agron <abrielleagron@MacBook-Air.attlocal.net>
2024-01-27 15:12:53 -0600

    more distance debug

--- examples/mapping.py
+++ examples/mapping.py
@@ -7,10 +7,13 @@ angle = 10
 def main():
     while(True):
         for i in range (-60, 61, 60):
-            time.sleep(1)
             distance = fc.get_distance_at(angle + i)
             print(str(i), str(distance))
             time.sleep(1)
+        for i in range (10):
+            distance = fc.get_distance_at(10)
+            print(str(i), str(distance))
+            time.sleep(.5)
 if __name__ == "__main__":
     try: 
         main()
