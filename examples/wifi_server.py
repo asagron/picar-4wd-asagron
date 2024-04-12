3b9ff2da735a8e0bdbb4a1329faa96079ef3602e
asagron <46514929+asagron@users.noreply.github.com>
2024-04-12 08:24:28 -0700

    Update wifi_server.py

--- examples/wifi_server.py
+++ examples/wifi_server.py
@@ -8,11 +8,11 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
     s.listen()
 
-     def Keyboard_control(key):
+    def Keyboard_control(key):
         while True:
             global power_val
             #key=readkey()
-            if key== b"87
+            if key== b"87":
                 print("forward")
                 fc.forward(power_val)
             elif key==b"83":
@@ -29,21 +29,3 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             if key==b"q":
                 print("quit")  
                 break  
-
-
-
-    try:
-        while 1:
-            client, clientInfo = s.accept()
-            print("server recv from: ", clientInfo)
-            data = client.recv(1024)      # receive 1024 Bytes of message in binary format
-            if data != b"":
-                print(data)
-                Keyboard_control(data)
-                client.sendall(data) # Echo back to client
-    except: 
-        print("Closing socket")
-        client.close()
-        s.close()    
-
-   
