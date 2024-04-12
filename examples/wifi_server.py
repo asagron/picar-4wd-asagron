358c6dfffe78594ebe9c8491c03f3e42590f6e3b
asagron <46514929+asagron@users.noreply.github.com>
2024-04-12 09:06:50 -0700

    Update wifi_server.py

--- examples/wifi_server.py
+++ examples/wifi_server.py
@@ -1,7 +1,9 @@
 import socket
 import picar_4wd as fc
+from picar_4wd.adc import ADC
 
 power_val = 50
+
 HOST = "192.168.1.143" # IP address of your Raspberry PI
 PORT = 65432          # Port to listen on (non-privileged ports are > 1023)
 
@@ -13,23 +15,41 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         global power_val
         #key=readkey()
         print(key)
-        if key== b"87":
-            print("forward")
+        if key== b'87\r\n':
+            direction = "forward"
             fc.forward(power_val)
-        elif key==b"83":
-            print("left")
+        elif key==b'83\r\n':
+            direction = "left"
             fc.turn_left(power_val)
-        elif key==b"65":
-            print("backward")
+        elif key==b'65\r\n':
+            direction = "backward"
             fc.backward(power_val)
-        elif key==b"68":
-            print("right")
+        elif key==b'68\r\n':
+            direction = "right"
             fc.turn_right(power_val)
         else:
             fc.stop()
-        if key==b"q":
-            print("quit")  
-            break  
+            direction = "N/A"
+        if key==b'q\r\n':
+            direction = "N/A"
+        return direction
+
+    def power_read():
+        power_read_pin = ADC('A4')
+        power_val = power_read_pin.read()
+        power_val = power_val / 4095.0 * 3.3
+        # print(power_val)
+        power_val = power_val * 3
+        power_val = round(power_val, 2)
+        return power_val
+
+    def cpu_temperature():          # cpu_temperature
+        raw_cpu_temperature = subprocess.getoutput("cat /sys/class/thermal/thermal_zone0/temp")
+        cpu_temperature = round(float(raw_cpu_temperature)/1000,2)               # convert unit
+        #cpu_temperature = 'Cpu temperature : ' + str(cpu_temperature)
+        return cpu_temperature
+
+
     try:
         while 1:
             client, clientInfo = s.accept()
@@ -37,9 +57,15 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             data = client.recv(1024)      # receive 1024 Bytes of message in binary format
             if data != b"":
                 print(data)
-                Keyboard_control(data)
-                client.sendall(data) # Echo back to client
+                direction = Keyboard_control(data)
+                print(direction)
+                power_val = power_read()
+                print(power_val)
+                cpu_temp = cpu_temperature()
+                print(cpu_temp)
+                send_back = [direction, power_val, cpu_temp]
+                client.sendall(send_back) # Echo back to client
     except: 
         print("Closing socket")
         client.close()
-        s.close()    
+        s.close()   
