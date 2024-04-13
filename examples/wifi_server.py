39df6fb69c3ea820a9fdb7921cde56fa0903bd5b
Colton Gann <44731467+Colton9810@users.noreply.github.com>
2024-04-13 22:47:49 +0100

    Update wifi_server.py

--- examples/wifi_server.py
+++ examples/wifi_server.py
@@ -2,9 +2,8 @@ import socket
 import picar_4wd as fc
 from picar_4wd.adc import ADC
 
-power_val = 50
-
-HOST = "192.168.1.143" # IP address of your Raspberry PI
+speed = 50
+HOST = "192.168.1.79" # IP address of your Raspberry PI
 PORT = 65432          # Port to listen on (non-privileged ports are > 1023)
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
@@ -12,33 +11,25 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.listen()
 
     def Keyboard_control(key):
-        global power_val
-        #key=readkey()
-        print(key)
-        if key== b'87\r\n':
-            direction = "forward"
-            fc.forward(power_val)
-        elif key==b'83\r\n':
-            direction = "left"
-            fc.turn_left(power_val)
-        elif key==b'65\r\n':
-            direction = "backward"
-            fc.backward(power_val)
-        elif key==b'68\r\n':
-            direction = "right"
-            fc.turn_right(power_val)
+        global speed
+        
+        if key== b'Forward\r\n':
+            fc.forward(speed)
+        elif key==b'Left\r\n':
+            fc.turn_left(speed)
+        elif key==b'Backward\r\n':
+            fc.backward(speed)
+        elif key==b'Right\r\n':
+            fc.turn_right(speed)
+        elif key==b'Quit\r\n':
+            fc.stop()
         else:
             fc.stop()
-            direction = "N/A"
-        if key==b'q\r\n':
-            direction = "N/A"
-        return direction
 
     def power_read():
         power_read_pin = ADC('A4')
         power_val = power_read_pin.read()
         power_val = power_val / 4095.0 * 3.3
-        # print(power_val)
         power_val = power_val * 3
         power_val = round(power_val, 2)
         return power_val
@@ -57,15 +48,16 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             data = client.recv(1024)      # receive 1024 Bytes of message in binary format
             if data != b"":
                 print(data)
-                direction = Keyboard_control(data)
-                print(direction)
-                power = power_read()
-                print(power)
-                #temp = cpu_temperature()
-                #print(temp)
+                Keyboard_control(data)
                 client.sendall(data) # Echo back to client
+                
+            power = power_read()
+            power = str(power)
+            print(power)
+            client.send(power)
+
     except: 
         print("Closing socket")
+        fc.stop()
         client.close()
         s.close()   
-
